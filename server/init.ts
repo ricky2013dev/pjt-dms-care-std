8798f883139af0a3adff06929d9e43168a8388d5
Ricky Kim <rickykim2013.dev@gmail.com>
2026-01-19 17:41:49 -0600

    fix deploy

--- server/init.ts
+++ server/init.ts
@@ -1,9 +1,6 @@
 import dotenv from "dotenv";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { resolve } from "path";
 
 // Determine which env file to load based on environment or dotenv_config_path
 const envFile = process.env.dotenv_config_path || ".env.local";
-dotenv.config({ path: resolve(__dirname, "..", envFile) });
+dotenv.config({ path: resolve(process.cwd(), envFile) });
